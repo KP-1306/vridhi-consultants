2020a245026d2db485a386a63ec832a463b0c9da
KP-1306 <kbisht786@gmail.com>
2025-11-29 23:19:18 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <div className="container footer-inner">
+        <div>© {year} VridhI Consultants. All rights reserved.</div>
+        <div>
+          <a href="#top">Back to top</a>
+          &nbsp;•&nbsp;
+          <span>vridhiconsultants.com</span>
+        </div>
+      </div>
+    </footer>
+  );
+}
 
