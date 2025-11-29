4fec66179f7f120a241e92e1e4fda74db32333a9
KP-1306 <kbisht786@gmail.com>
2025-11-29 23:17:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
 
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Services from "./pages/Services.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+
+export default function App() {
+  return (
+    <div className="app-root">
+      <Navbar />
+      <main id="top">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
