5e2c6759a3f7a86b0e8c6d6465756bd7d2eb2d74
KP-1306 <kbisht786@gmail.com>
2025-11-30 23:20:34 +0530

    Update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,12 @@
+// src/pages/Services.jsx
+
 import React from "react";
+import { Link } from "react-router-dom";
 import { services } from "../data/services.js";
 
 export default function Services() {
   return (
-    <section>
+    <section id="services">
       <div className="container section-inner">
         <div className="section-header">
           <div>
@@ -20,16 +23,22 @@ export default function Services() {
 
         <div className="services-grid">
           {services.map((svc) => (
-            <article key={svc.tag} className="service-card">
-              <div className="service-tag">{svc.tag}</div>
-              <div className="service-name">{svc.name}</div>
-              <p className="service-desc">{svc.desc}</p>
-              <ul className="service-list">
-                {svc.bullets.map((b, idx) => (
-                  <li key={idx}>{b}</li>
-                ))}
-              </ul>
-            </article>
+            <Link
+              key={svc.slug}
+              to={`/services/${svc.slug}`}
+              className="service-card-link"
+            >
+              <article className="service-card">
+                <div className="service-tag">{svc.tag}</div>
+                <div className="service-name">{svc.name}</div>
+                <p className="service-desc">{svc.desc}</p>
+                <ul className="service-list">
+                  {svc.bullets.map((b, idx) => (
+                    <li key={idx}>{b}</li>
+                  ))}
+                </ul>
+              </article>
+            </Link>
           ))}
         </div>
       </div>
